bf75b8f3029cf14e8773adde2d184a8c7422443d
juanarton <juanarton@protonmail.com>
2025-04-16 23:17:07 +0700

    Upgrade app version

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -9,7 +9,7 @@ plugins {
 
 android {
     val buildTime = System.currentTimeMillis()
-    val baseVersionName = "0.9.2"
+    val baseVersionName = "0.9.4"
     namespace = "com.mja.reyamf"
     compileSdk = 35
 
